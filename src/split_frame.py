94614c86784bf3cad366cd0af929ff0132ee11b2
mervedadas <mervedadas96@gmail.com>
2019-12-22 16:00:35 +0300

    Added to split frame for model

---
+++ src/split_frame.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class Frame:
+    def __init__(self, frame_size, width, height, dimension):
+        self.frame_size = frame_size
+        self.width = width
+        self.height = height
+        self.dimension = dimension
+
+    def frame(self, data):
+        data = np.array(data)
+        if data.shape[0] % self.frame_size == 0:
+            final = data.reshape(int(data.shape[0] / self.frame_size), self.frame_size, self.width, self.height,
+                                 self.dimension)
+        else:
+            overlap = (data.shape[0] - self.frame_size * int(data.shape[0] / self.frame_size))  # remain frame size / 16
+            beginning = int((self.frame_size - overlap) / 2)  # (16 - L) / 2
+            ending = self.frame_size - overlap - beginning  #
+            final = np.concatenate((data[:-overlap], np.concatenate(
+                (data[:beginning], np.concatenate((data[-overlap:], data[-ending:]))))))
+            final = final.reshape(int(final.shape[0] / self.frame_size), self.frame_size, self.width, self.height,
+                                  self.dimension)
+
+        return final
