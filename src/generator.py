99ce26e35c01f872b1f62e3e241841222ea2a1bf
Furkan <karadasfurkan@yandex.com>
2019-12-25 22:42:40 +0300

    Fixed bugs

--- src/generator.py
+++ src/generator.py
@@ -1,17 +1,23 @@
 import numpy as np
 from tensorflow.keras.utils import Sequence
 
-class Generator(Sequence):
 
-    def __init__(self, image_filenames, labels, batch_size):
-        self.image_filenames, self.labels = image_filenames, labels
+class Generator(Sequence):
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
         self.batch_size = batch_size
 
     def __len__(self):
-        return np.ceil(len(self.image_filenames) / float(self.batch_size))
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
 
     def __getitem__(self, idx):
-        batch_x = self.image_filenames[idx * self.batch_size:(idx + 1) * self.batch_size]
-        batch_y = self.labels[idx * self.batch_size:(idx + 1) * self.batch_size]
-        
-        return batch_x, batch_y
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        x = []
+        y = []
+        for file_index in range(len(batch_x)):
+            data = np.load(batch_x[file_index])
+            x.append(data)
+            y.extend([batch_y[file_index]] * data.shape[0])
+        return np.vstack(x), np.array(y)
--- src/main.py
+++ src/main.py
@@ -2,43 +2,18 @@ import numpy as np
 import os
 
 from model import C3D
-
-
-def batch_generator(X, Y, batch_size):
-    indices = np.arange(len(X))
-    batchs = []
-    while True:
-        np.random.shuffle(indices)
-        for i in indices:
-            batchs.append(i)
-            if(len(batchs) == batch_size):
-                data = []
-                label = []
-                for batch in batchs:
-                    value = np.load(X[batch])
-                    data.append(value)
-                    label.extend([Y[batch]] * value.shape[0])
-                yield np.vstack(data), np.vstack(label)
-                batch = []
-                data = []
-                label = []
+from generator import Generator
 
 
 def main():
     #os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-    batch_size = 1
-    train = batch_generator(np.load("dataPaths/X_train.npy"), np.load("dataPaths/Y_train.npy"), batch_size)
-    test = batch_generator(np.load("dataPaths/X_test.npy"), np.load("dataPaths/Y_test.npy"), batch_size)
 
+    batch_size = 1
     model = C3D(16, 227, 227, 3, 8).getModel()
     model.compile(optimizer="adam", loss='categorical_crossentropy', metrics=['accuracy'])
-    print(model.summary())
 
-    model.fit_generator(generator=train,
+    model.fit_generator(generator=Generator(np.load("dataPaths/X_train.npy"),np.load("dataPaths/Y_train.npy"), batch_size),
                         steps_per_epoch=int(np.load("dataPaths/X_train.npy").shape[0] // batch_size),
-                        epochs=10,
-                        validation_data=test,
-                        validation_steps=int(np.load("dataPaths/X_test.npy").shape[0] // batch_size))
-
+                        epochs=10)
 
 main()
