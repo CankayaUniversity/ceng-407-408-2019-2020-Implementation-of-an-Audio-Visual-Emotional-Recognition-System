37686d7438a244886434fc4236f0b00ccdf56546
Furkan <karadasfurkan@yandex.com>
2020-05-04 15:30:13 +0300

    Added validation for upload contents

--- content/models.py
+++ content/models.py
@@ -1,5 +1,8 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from django.core.exceptions import ValidationError
 
 LANGUAGES = (
     ('TR', 'Turkish'),
@@ -30,6 +33,23 @@ EMOTIONS = (
 )
 
 
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = ['.mp4', '.wav']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(((mark_safe(
+            '<p style="color:red;">Unsupported file!</p>'))))
+
+
+def validate_file_size(value):
+    filesize = value.size
+    if filesize > 10485760:
+        raise ValidationError(((mark_safe(
+            "<p style='color:red;'>The maximum file size that can be uploaded is 10MB!</p>"))))
+    else:
+        return value
+
+
 class Contents(models.Model):
     uploader = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
     content_name = models.CharField(max_length=50)
@@ -38,4 +58,5 @@ class Contents(models.Model):
     content_sex = models.CharField(max_length=5, choices=SEX, default=SEX[0])
     content_emotion = models.CharField(max_length=9, choices=EMOTIONS, default=EMOTIONS[0])
     commend = models.TextField()
-    content_upload = models.FileField(null=False, upload_to="contents/")
+    content_upload = models.FileField(null=False, upload_to="contents/",
+                                      validators=[validate_file_extension, validate_file_size])
